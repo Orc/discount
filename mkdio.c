a056718addbc044456b507887de6d2f807332d0a
david parsons <orc@pell.portland.or.us>
2008-07-25 18:18:32 -0700

    Back out the inadvertantly backed out changes to mkdio.c

--- mkdio.c
+++ mkdio.c
@@ -80,7 +80,7 @@ snip(Line *p)
 
 /* build a Document from any old input.
  */
-typedef unsigned int (*getc_func)(void*);
+typedef int (*getc_func)(void*);
 
 Document *
 populate(getc_func getc, void* ctx, int flags)
@@ -156,7 +156,7 @@ struct string_ctx {
 } ;
 
 
-static char
+static int
 strget(struct string_ctx *in)
 {
     if ( !in->size ) return EOF;
