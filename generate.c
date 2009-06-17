e4ba09cd54b7e6477b0b89e50c68ec62b55738d1
David Parsons <orc@crepe.pell.portland.or.us>
2009-06-17 16:20:59 -0700

    Use the existing reddit-style autolink termination characters instead of the ones I'd guessed about.

--- generate.c
+++ generate.c
@@ -899,7 +899,7 @@ maybe_autolink(MMIOT *f)
 	     if ( peek(f, size+2) != EOF )
 		++size;
 	}
-	else if ( !(isalnum(c) || strchr(",;+-/:._%~@&?=(", c) || (c & 0x80)) )
+	else if ( isspace(c) || strchr("'\"()[]{}<>`", c) )
 	    break;
 
     if ( (size > 1) && process_possible_link(f, size) ) {
