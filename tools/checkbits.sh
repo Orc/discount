dec1515923b521f5f857caa79ae6f12225928b42
david parsons <orc@pell.portland.or.us>
2010-10-22 14:09:05 -0700

    checkbits.sh: don't exec the diff, because that means that the workfiles remain after the program finishes.

--- tools/checkbits.sh
+++ tools/checkbits.sh
@@ -5,4 +5,7 @@ trap "rm -f in.markdown.h in.mkdio.h" EXIT
 grep '#define MKD_' markdown.h | awk '$3 ~ /0x0/ {print $2,$3;}' > in.markdown.h
 grep '#define MKD_' mkdio.h | awk '$3 ~ /0x0/ {print $2,$3;}' > in.mkdio.h
 
-exec diff -c -bw in.markdown.h in.mkdio.h
+diff -c -bw in.markdown.h in.mkdio.h
+retcode=$?
+
+exit $retcode
