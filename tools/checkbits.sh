456f4b873d5a44dd851aca8fb6f580f39357d99e
david parsons <orc@pell.portland.or.us>
2010-10-22 13:43:01 -0700

    Add a `verify` stanza to the Makefile to compare the option bits in markdown.h and mkdio.h

---
+++ tools/checkbits.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+trap "rm -f in.markdown.h in.mkdio.h" EXIT
+
+grep '#define MKD_' markdown.h | awk '$3 ~ /0x0/ {print $2,$3;}' > in.markdown.h
+grep '#define MKD_' mkdio.h | awk '$3 ~ /0x0/ {print $2,$3;}' > in.mkdio.h
+
+exec diff -c -bw in.markdown.h in.mkdio.h
