c7d8a6e2dda9315cd49fe24d2d1a17d4d6c98aa1
Koen Punt <koen@koenpunt.nl>
2015-10-25 02:07:33 +0200

    handle errors in mkd_xhtmlpage

--- xmlpage.c
+++ xmlpage.c
@@ -22,25 +22,31 @@ mkd_xhtmlpage(Document *p, int flags, FILE *out)
     extern char *mkd_doc_title(Document *);
     
     if ( mkd_compile(p, flags) ) {
-	fprintf(out, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-	fprintf(out, "<!DOCTYPE html "
-		     " PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\""
-		     " \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n");
+        int ret = 0;
 
-	fprintf(out, "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n");
+#define MKD_ERRH(s) ret |= (s) < 0 ? -1 : 0
 
-	fprintf(out, "<head>\n");
-	if ( title = mkd_doc_title(p) )
-	    fprintf(out, "<title>%s</title>\n", title);
-	mkd_generatecss(p, out);
-	fprintf(out, "</head>\n");
-	
-	fprintf(out, "<body>\n");
-	mkd_generatehtml(p, out);
-	fprintf(out, "</body>\n");
-	fprintf(out, "</html>\n");
+        MKD_ERRH( fprintf(out, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n") );
+        MKD_ERRH( fprintf(out, "<!DOCTYPE html "
+             " PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\""
+             " \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n") );
+        
+        MKD_ERRH( fprintf(out, "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n") );
+        
+        MKD_ERRH( fprintf(out, "<head>\n") );
+        if ( title = mkd_doc_title(p) )
+            MKD_ERRH( fprintf(out, "<title>%s</title>\n", title) );
+        MKD_ERRH( mkd_generatecss(p, out) );
+        MKD_ERRH( fprintf(out, "</head>\n") );
+        
+        MKD_ERRH( fprintf(out, "<body>\n") );
+        MKD_ERRH( mkd_generatehtml(p, out) );
+        MKD_ERRH( fprintf(out, "</body>\n") );
+        MKD_ERRH( fprintf(out, "</html>\n") );
 
-	return 0;
+#undef MKD_ERRH
+
+        return ret;
     }
     return -1;
 }
