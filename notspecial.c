903a76ef84866675c0a8c5e98d33a241fb9f80d7
david parsons <orc@pie.pell.portland.or.us>
2017-02-04 23:55:35 -0800

    Utility function for theme & mkd2html -- check a filename to see if it's a special file that doesn't need to be deleted or to have a .html suffix added to it.   It only works on machines that have a stat() system call, otherwise it thinks that nothing is special.

---
+++ notspecial.c
@@ -0,0 +1,28 @@
+/*
+ * check a filename to see if it's a (fifo|character special|socket) object
+ * (if a stat() function doesn't exist, we can't stat so we'll just return
+ *  false no matter what.)
+ */
+
+#include "config.h"
+
+#if HAVE_STAT
+#include <sys/stat.h>
+
+int
+notspecial(char *file)
+{
+    struct stat info;
+
+    if ( stat(file, &info) != 0 )
+	return 0;
+    
+    return (info.st_flags & (S_IFIFO|S_IFCHR|S_IFSOCK));
+}
+#else
+int
+notspecial(char *file)
+{
+    return 0;
+}
+#endif
