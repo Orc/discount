6b3398cfa814277e80635e38e1ecd0e5bfa27b08
david parsons <orc@pell.portland.or.us>
2020-08-23 22:45:16 -0700

    backport the label size from v3

--- mkdio.c
+++ mkdio.c
@@ -232,7 +232,7 @@ mkd_anchor_format(char *s, int len, int labelformat, mkd_flag_t flags)
     int h4anchor = !is_flag_set(flags, MKD_URLENCODEDANCHOR);
     static const unsigned char hexchars[] = "0123456789abcdef";
 
-    needed = labelformat ? (4*len) : len;
+    needed = (labelformat ? (4*len) : len) + 2 /* 'L', trailing null */;
 
     if ( (res = malloc(needed)) == NULL )
 	return NULL;
