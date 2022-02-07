130af993fa240fd32c08805781274a1f7bd94b81
Kaz KOBARA <M-kaz-ml@aist.go.jp>
2022-02-07 11:26:37 +0900

    Fix 'make test' error for BusyBox

--- tools/checkbits.sh
+++ tools/checkbits.sh
@@ -1,13 +1,24 @@
 #! /bin/sh
 
-trap "rm -f in.markdown.h in.mkdio.h" EXIT
+trap "rm -f in.markdown.h in.mkdio.h in.diff_markdown_mkdio.log" EXIT
 
 grep 'MKD_' markdown.h | sed -ne '1,/}/p' > in.markdown.h
 grep 'MKD_' mkdio.h | sed -ne '1,/}/p' > in.mkdio.h
 
-diff -c -bw in.markdown.h in.mkdio.h
+diff -c -bw in.markdown.h in.mkdio.h 2> in.diff_markdown_mkdio.log
 retcode=$?
 
-[ $retcode -eq 0 ] || echo "markdown flags differ between markdown.h & mkdio.h!" 1>&2
+# Retry without '-c' option for BusyBox at least v1.34
+# that does not support it and returns '1' for the option error.
+if [ $retcode -ne 0 ]; then
+        if grep -iq "BusyBox" in.diff_markdown_mkdio.log; then
+                echo "--- stderr log w/o -c option ---" >> in.diff_markdown_mkdio.log
+                diff -bw in.markdown.h in.mkdio.h      2>> in.diff_markdown_mkdio.log; retcode=$?
+                echo "--------------------------------" >> in.diff_markdown_mkdio.log
+        fi
+fi
+
+# If still not '0'
+[ $retcode -eq 0 ] || (cat in.diff_markdown_mkdio.log; echo "markdown flags differ between markdown.h & mkdio.h!" 1>&2)
 
 exit $retcode
