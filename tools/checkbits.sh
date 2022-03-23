b0c441594790a808d44c302c674792d06a5d11fe
david parsons <orc@pell.portland.or.us>
2022-03-22 17:30:37 -0700

    don't use diff -c (Busybox diff doesn't support it) but just dump the diff output to /dev/null

--- tools/checkbits.sh
+++ tools/checkbits.sh
@@ -5,7 +5,7 @@ trap "rm -f in.markdown.h in.mkdio.h" EXIT
 grep 'MKD_' markdown.h | sed -ne '1,/}/p' > in.markdown.h
 grep 'MKD_' mkdio.h | sed -ne '1,/}/p' > in.mkdio.h
 
-diff -c -bw in.markdown.h in.mkdio.h
+diff -bw in.markdown.h in.mkdio.h >/dev/null 2>&1
 retcode=$?
 
 [ $retcode -eq 0 ] || echo "markdown flags differ between markdown.h & mkdio.h!" 1>&2
