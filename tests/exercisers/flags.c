8a04e311055f4f27345c12117ccbaa57171b076e
david parsons <orc@pell.portland.or.us>
2022-05-13 21:55:54 -0700

    add some tests against null flag parameters

--- tests/exercisers/flags.c
+++ tests/exercisers/flags.c
@@ -6,19 +6,28 @@ int
 main()
 {
     mkd_flag_t *flags = mkd_flags();
-    mkd_flag_t *copy;
 
 
-    fprintf(stderr, "exercise flags: ");
+    fprintf(stderr, "check flag functions: ");
     fprintf(stderr, "set ");
     mkd_set_flag_num(flags, MKD_DLDISCOUNT);
+
+    fprintf(stderr, "set(0) ");
+    mkd_set_flag_num(0, MKD_DLDISCOUNT);
 
     fprintf(stderr, "clear ");
     mkd_clr_flag_num(flags, MKD_DLDISCOUNT);
 
+    fprintf(stderr, "clear(0) ");
+    mkd_clr_flag_num(0, MKD_DLDISCOUNT);
+
     fprintf(stderr, "copy ");
 
-    copy = mkd_copy_flags(flags);
+    (void)mkd_copy_flags(flags);
+
+    fprintf(stderr, "copy(0) ");
+
+    (void)mkd_copy_flags(0);
 
     fprintf(stderr, "ok\n");
     exit(0);
